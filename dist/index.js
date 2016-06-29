09c447babc38472ac25ac9d288811c3da94665b3
Sönke Kluth <soenke.kluth@gmail.com>
2016-06-29 15:50:12 +0200

    add dist js

---
+++ dist/index.js
@@ -0,0 +1,1 @@
+'use strict';var _something=require('./something');var _something2=_interopRequireDefault(_something);function _interopRequireDefault(obj){return obj&&obj.__esModule?obj:{default:obj}}var someThing=new _something2.default;var anotherThing=new _something.AnotherThing;document.getElementById('output').innerHTML=someThing.name+' & '+anotherThing.name;
