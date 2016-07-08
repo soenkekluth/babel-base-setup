c86fe7ece377b2699fcdbcca10d7b588a6d640c0
Sönke Kluth <mail@soenkekluth.com>
2016-07-08 03:32:46 +0200

    rm old js files

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import Something, { AnotherThing } from './something';
-
-const someThing = new Something();
-const anotherThing = new AnotherThing();
-
-document.getElementById('output').innerHTML = someThing.name + ' & ' + anotherThing.name;
